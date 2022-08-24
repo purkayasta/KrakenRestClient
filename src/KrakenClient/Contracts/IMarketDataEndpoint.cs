cc0fb40922c3751563b13baf7e5af4bc9552ab8b
purkayasta <pritompurkayasta@gmail.com>
2022-08-25 01:46:17 +0600

    #8 recent spread endpoint added

--- src/KrakenClient/Contracts/IMarketDataEndpoint.cs
+++ src/KrakenClient/Contracts/IMarketDataEndpoint.cs
@@ -5,18 +5,13 @@ namespace KrakenClient.Contracts;
 public interface IMarketDataEndpoint
 {
     Task<ServerTime?> GetServerTime();
-
     Task<SystemStatus?> GetSystemStatus();
-
     Task<AssetInfo?> GetAssetInfo();
     Task<AssetInfo?> GetAssetInfo(string asset, string aclass = "currency");
-
     Task<TradableAssetPair?> GetTradableAssetPair(string assetPair);
-
     Task<TickerInformation?> GetTickerInformation(string pair);
-
     Task<OHLCData?> GetOhlcData(string pair, int? since = null, int interval = 1);
-
     Task<OrderBook?> GetOrderbook(string pair, int count = 2);
     Task<RecentTrades?> GetRecentTrades(string pair, int? since = null);
+    Task<RecentSpreads?> GetRecentSpreads(string pair, int? since = null);
 }
---
+++ src/KrakenClient/Endpoints/MarketData/MarketDataEndpoint.RecentSpread.cs
@@ -0,0 +1,20 @@
+﻿using KrakenClient.Contracts;
+using KrakenClient.Models.MarketData;
+
+namespace KrakenClient.Endpoints.MarketData;
+
+internal partial class MarketDataEndpoint : IMarketDataEndpoint
+{
+    private const string RecentSpreadUrl = "/Spread";
+
+    public Task<RecentSpreads?> GetRecentSpreads(string pair, int? since = null)
+    {
+        ArgumentNullException.ThrowIfNull(pair, nameof(pair));
+
+        _httpClient.BodyParameters.Add("pair", pair);
+
+        if (since is not null) _httpClient.BodyParameters.Add("since", since.Value.ToString());
+
+        return _httpClient.Get<RecentSpreads>(BaseUrl + RecentSpreadUrl);
+    }
+}
