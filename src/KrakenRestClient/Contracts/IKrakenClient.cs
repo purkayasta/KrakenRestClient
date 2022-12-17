87d2847dd7d3c9b622f8b93cc98f989b829bf985
purkayasta <pritompurkayasta@gmail.com>
2022-12-17 19:42:35 +0600

    added user funding endpoint

--- src/KrakenRestClient/Contracts/IKrakenClient.cs
+++ src/KrakenRestClient/Contracts/IKrakenClient.cs
@@ -5,4 +5,5 @@ public interface IKrakenClient
     public IMarketDataEndpoint MarketData();
     public IUserDataEndpoint UserData();
     public IUserTradingEndpoint UserTrading();
+    public IUserFundingEndpoint UserFunding();
 }
--- src/KrakenRestClient/KrakenClient.cs
+++ src/KrakenRestClient/KrakenClient.cs
@@ -1,6 +1,7 @@
 ﻿using KrakenRestClient.Core;
 using KrakenRestClient.Endpoints.MarketData;
 using KrakenRestClient.Endpoints.UserData;
+using KrakenRestClient.Endpoints.UserFunding;
 using KrakenRestClient.Endpoints.UserTrading;
 
 namespace KrakenRestClient;
@@ -10,15 +11,18 @@ internal sealed class KrakenClient : IKrakenClient
     private readonly Lazy<IMarketDataEndpoint> _marketDataEndpoint;
     private readonly Lazy<IUserDataEndpoint> _userDataEndpoint;
     private readonly Lazy<IUserTradingEndpoint> _userTradingEndpoint;
+    private readonly Lazy<IUserFundingEndpoint> _userFundingEndpoint;
 
     public KrakenClient(IKrakenHttpClient httpClient)
     {
         _marketDataEndpoint = new Lazy<IMarketDataEndpoint>(() => new MarketDataEndpoint(httpClient));
         _userDataEndpoint = new Lazy<IUserDataEndpoint>(() => new UserDataEndpoint(httpClient));
         _userTradingEndpoint = new Lazy<IUserTradingEndpoint>(() => new UserTradingEndpoint(httpClient));
+        _userFundingEndpoint = new Lazy<IUserFundingEndpoint>(() => new UserFundingEndpoint(httpClient));
     }
 
     public IMarketDataEndpoint MarketData() => _marketDataEndpoint.Value;
     public IUserDataEndpoint UserData() => _userDataEndpoint.Value;
     public IUserTradingEndpoint UserTrading() => _userTradingEndpoint.Value;
+    public IUserFundingEndpoint UserFunding() => _userFundingEndpoint.Value;
 }
