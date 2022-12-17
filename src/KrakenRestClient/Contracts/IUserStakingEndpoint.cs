30161c89ce8ae788dc2a1e3f77b70d40aa670f35
purkayasta <pritompurkayasta@gmail.com>
2022-12-17 20:06:25 +0600

    #20 Added stakeasset API

--- src/KrakenRestClient/Contracts/IUserStakingEndpoint.cs
+++ src/KrakenRestClient/Contracts/IUserStakingEndpoint.cs
@@ -1,6 +1,16 @@
-﻿namespace KrakenRestClient.Contracts;
+﻿using KrakenRestClient.Models.UserStaking;
+
+namespace KrakenRestClient.Contracts;
 
 public interface IUserStakingEndpoint
 {
-    
+    /// <summary>
+    /// Stake an asset from your spot wallet. This operation requires an API key with Withdraw funds permission.
+    /// </summary>
+    /// <param name="asset">Asset to stake (asset ID or altname)</param>
+    /// <param name="amount">Amount of the asset to stake</param>
+    /// <param name="method">Name of the staking option to use (refer to the Staking Assets endpoint for the correct
+    /// method names for each asset)</param>
+    /// <returns></returns>
+    Task<StakeAssetResponse?> StakeAssetAsync(string asset, string amount, string method);
 }
